704c62efd6676a543e60d75b4e2eea570256860d
nandev16 <marcoferab2001@gmail.com>
2023-09-19 10:36:13 -0600

    :tada: New portfolio ready for deploy

---
+++ src/config.ts
@@ -0,0 +1,3 @@
+
+export const SITE_TITLE = 'Nandev - Portfolio';
+export const SITE_DESCRIPTION = 'Sitio web personal con mi portafolio y CV donde muestro quien soy, mis proyectos y mi blog personal.';
