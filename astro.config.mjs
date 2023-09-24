2f31b5f9e5f774d6a0eed907f890cb5b0ce9e3b3
nandev16 <marcoferab2001@gmail.com>
2023-09-23 23:19:54 -0600

    :rocket: Redeploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,16 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   site: "https://nandev.xyz",
-  integrations: [mdx(), sitemap(), tailwind(), react()]
+  integrations: [mdx(), sitemap(), tailwind(), react()],
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
